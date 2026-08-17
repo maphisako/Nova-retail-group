41bd59e5e7b9a8a99ce0f2cd51d9b56d2e465724
maphisako <maphisako@gmail.com>
2026-08-17 22:54:03 +0200

    Update and rename question_09.sql to question_2.4.sql

--- sql/question_09.sql
+++
@@ -1,7 +0,0 @@
-select c.channel,count(s.orderid) as number_of_orders,avg(p.unitprice) as average_order_value,sum(s.totalsales-p.costprice) as total_revenue
-from customers c
-inner join sales s
-on c.customerid=s.customerid
-inner join products p
-on s.productid=p.productid
-group by s.orderid,p.unitprice,s.totalsales-p.costprice,c.channel
