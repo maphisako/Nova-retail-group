dae8e7676f9a885e14c694e4811a9c1f33f2a337
maphisako <maphisako@gmail.com>
2026-08-17 23:35:39 +0200

    Update question_3.2.sql

--- sql/question_3.2.sql
+++ sql/question_3.2.sql
@@ -1,9 +1,9 @@
-select sum(totalsales) as total_purchases,count(s.orderid) as number_of_orders,avg(s.unitprice) as average_order_value,c.region,c.channel
+select sum(totalsales) as total_purchases,count(s.orderid) as number_of_orders,avg(s.unitprice) as average_order_value,c.region,c.channel,c.customerid
 from customers c
 inner join sales s
 on c.customerid=s.customerid
 inner join products p
 on p.productid=s.productid
-group by c.region,c.channel
+group by c.region,c.channel,c.customerid
 having count(s.orderid)>3
 order by total_purchases desc
