b75c596f8489914b14dcb832e3b5bab5df664b4a
maphisako <maphisako@gmail.com>
2026-09-06 19:13:41 +0200

    Update question_2.2.sql

--- sql/question_2.2.sql
+++ sql/question_2.2.sql
@@ -1,4 +1,4 @@
-select concat(c.FirstName,' ',c.LastName) as full_name, sum(s.totalsales) as total_purchase_amount, c.customerid, dense_rank() over (order by sum(s.totalsales) desc) as customer_rank
+select concat(c.FirstName,' ',c.LastName) as full_name, sum(s.totalsales) as total_purchase_amount, c.customerid,
 from sales s
 inner join customers c
 on s.customerid=c.customerid
