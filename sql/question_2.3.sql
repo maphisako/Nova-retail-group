05839b7251971264111d3ea8824f820123991050
maphisako <maphisako@gmail.com>
2026-09-06 19:16:15 +0200

    Update question_2.3.sql

--- sql/question_2.3.sql
+++ sql/question_2.3.sql
@@ -1,5 +1,5 @@
-select total_sales,year(orderdate) as year,month(orderdate) as month
+select sum(Totalsales) as total_sales,year(orderdate) as year,month(orderdate) as month
 from sales
-  where orderdate='2024'
+  where year(orderdate)='2024'
   group by 1,2,3
 order by year,month
