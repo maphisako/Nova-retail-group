29ffb8ce33f1a9365d89aa8500f41dd073665cd4
maphisako <maphisako@gmail.com>
2026-09-06 19:29:32 +0200

    Update question_3.4.sql

--- sql/question_3.4.sql
+++ sql/question_3.4.sql
@@ -1,6 +1,17 @@
-with annual_sales as(select year(OrderDate) as year,sum(totalsales) as sales_revenue 
-from sales 
-where year(OrderDate) in (2024,2023)
-group by OrderDate)
-select year,sales_revenue,lag(sales_revenue)over(order by year) as last_term_sales
-from annual_sales
+WITH yearly_sales AS (
+  SELECT 
+    YEAR(OrderDate) AS Year,
+    SUM(TotalSales) AS Total_Sales
+  FROM sales_2
+  GROUP BY YEAR(OrderDate)
+)
+SELECT 
+  y1.Year AS Year_2023,
+  y1.Total_Sales AS Sales_2023,
+  y2.Year AS Year_2024,
+  y2.Total_Sales AS Sales_2024,
+  ROUND(((y2.Total_Sales - y1.Total_Sales) / y1.Total_Sales) * 100, 2) AS Growth_Percent
+FROM yearly_sales y1
+CROSS JOIN yearly_sales y2
+WHERE y1.Year = 2023 AND y2.Year = 2024
+
