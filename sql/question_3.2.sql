9b2eb444e5f1ad09bd2a51ea39a0a6e95e5d5d22
maphisako <maphisako@gmail.com>
2026-09-06 19:27:42 +0200

    Update question_3.2.sql

--- sql/question_3.2.sql
+++ sql/question_3.2.sql
@@ -1,9 +1,15 @@
-select sum(totalsales) as total_purchases,count(s.orderid) as number_of_orders,avg(s.totalsales) as average_order_value,c.region,c.channel,c.customerid
-from customers c
-inner join sales s
-on c.customerid=s.customerid
-inner join products p
-on p.productid=s.productid
-group by c.region,c.channel,c.customerid
-having count(s.orderid)>3
-order by total_purchases desc
+SELECT 
+  c.CustomerID,
+  CONCAT(c.FirstName, ' ', c.LastName) AS CustomerName,
+  c.Region,
+  c.Channel AS Primary_Channel,
+  SUM(s.TotalSales) AS Total_Purchases,
+  COUNT(DISTINCT s.OrderID) AS Number_of_Orders,
+  ROUND(SUM(s.TotalSales) / COUNT(DISTINCT s.OrderID), 2) AS Average_Order_Value
+FROM sales_2 s
+INNER JOIN customers_2 c ON s.CustomerID = c.CustomerID
+GROUP BY c.CustomerID, c.FirstName, c.LastName, c.Region, c.Channel
+HAVING COUNT(DISTINCT s.OrderID) > 3
+ORDER BY Total_Purchases DESC
+
+
